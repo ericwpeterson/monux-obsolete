afb6ed200a965a099a51ed4baf2dab385f556ebc
Eric Peterson <erockp1@gmail.com>
2017-02-03 13:34:24 -0600

    added batch feature to server.

--- examples/react-d3-boxplot/server/src/server.js
+++ examples/react-d3-boxplot/server/src/server.js
@@ -6,6 +6,7 @@ import monObjectFactory from '../monobjects/factory';
 import monObjectServer from './monobjectserver';
 import { initFactory } from './monobjectserver';
 import { createStatsObject } from './stats';
+import async from 'async';
 
 let monObject = createStatsObject();
 
@@ -19,6 +20,61 @@ export default function startServer() {
     io.on('connection', function(socket) {
         socket.on('disconnect', function() {
             monObjectFactory.removeSocket(socket);
+        });
+
+        socket.on('Batch', function(requests) {
+
+            async.eachSeries(requests, function(request, cb) {
+
+                switch ( request.command ) {
+                    case "Get":
+                        monObjectServer.get(request.request, socket, function(err, res) {
+                            if (err) {
+                                console.log(err, res);
+                            }
+                            cb();
+                        });
+
+                    case "Set":
+                        monObjectServer.set(request.request, socket, function(err, res) {
+                            if (err) {
+                                console.log(err, res);
+                            }
+                            cb();
+                        });
+
+                    case "Call":
+                        monObjectServer.call(request.request, socket, function(err, res) {
+                            if (err) {
+                                console.log(err, res);
+                            }
+                            cb();
+                        });
+
+                    case "Watch":
+                        monObjectServer.watch(request.request, socket, function(err, res) {
+                            if (err) {
+                                console.log(err, res);
+                            }
+                            cb();
+                        });
+
+                    case "UnWatch":
+                        monObjectServer.unWatch(request.request, socket, function(err, res) {
+                            if (err) {
+                                console.log(err, res);
+                            }
+                        });
+
+                    default:
+                        //unknown command, just continue along
+                        cb();
+                }
+
+            }, function( err, ret ) {
+                //intentionally not returning anything back
+            });
+
         });
 
         socket.on('Get', function(request) {
